78d82e2c28aec4cfadbd48964f2359bdf447e74f
430lyj <430lyj@gmail.com>
2021-11-22 17:51:17 +0900

    feat : 사용자, 식당 및 메뉴 DB 설정

---
+++ imundongmukjjang/restaurants/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from accounts.models import CustomUser
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=60, null=False)
+    phone = models.CharField(max_length=20, null=True)
+    rating = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=255)
+    owner = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Menu_Price(models.Model):
+    menu = models.CharField(max_length=45)
+    price = models.IntegerField()
+    restaurant = models.ForeignKey('Restaurant', on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.menu
