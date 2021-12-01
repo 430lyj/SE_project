64857af573f5f3cf295fa2bd2974fe9564d4c04a
p-seungri <qkrtmdfl1103@naver.com>
2021-12-01 23:13:38 +0900

    Submission error corrected

--- imundongmukjjang/accounts/views.py
+++ imundongmukjjang/accounts/views.py
@@ -8,15 +8,18 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == "POST":
-        if CustomUser.objects.filter(username = request.POST['username']).exists(): # 이미 등록된 회원일 경우
-            return render(request, "signup.html", {"validity": 0})
-        elif request.POST["password1"] == request.POST["password2"]:    # 회원가입 정상 진행
-            user = CustomUser.objects.create_user(
-                username=request.POST["username"], password=request.POST["password1"], biz_registration=request.FILES["uploadedFile"])
-            auth.login(request, user)
-            return redirect('home')
-        else:
-            return render(request, "signup.html", {"validity": 1})  # 입력한 비밀번호 두 개가 일치하지 않을 경우
+        try:
+            if CustomUser.objects.filter(username = request.POST['username']).exists(): # 이미 등록된 회원일 경우
+                return render(request, "signup.html", {"validity": 0})
+            elif request.POST["password1"] == request.POST["password2"]:    # 회원가입 정상 진행
+                user = CustomUser.objects.create_user(
+                    username=request.POST["username"], password=request.POST["password1"], biz_registration=request.FILES["uploadedFile"])
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                return render(request, "signup.html", {"validity": 1})  # 입력한 비밀번호 두 개가 일치하지 않을 경우
+        except Exception:
+            return render(request, "signup.html", {"filled": 0})
     return render(request, 'signup.html')
 
 
