49725ed34deb87dc0ea1cc64c26546b127713d6c
430lyj <430lyj@gmail.com>
2021-12-11 14:45:06 +0900

    fix : 홈화면에서 정보 수정 페이지로 이동 문제 해결, is_assured되지 않은 사용자 접근제한

--- imundongmukjjang/restaurants/admin.py
+++ imundongmukjjang/restaurants/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Restaurant, Category, Menu_Price
+from .models import Big_Category, Restaurant, Category, Menu_Price
 
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Category)
 admin.site.register(Menu_Price)
+admin.site.register(Big_Category)
